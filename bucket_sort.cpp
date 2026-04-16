4ec29d3e3a736e2f86b3e19056efcfbddbfadaac
Saadetatabay <etatabaysaad143@gmail.com>
2026-04-16 19:04:13 +0300

    implement bucket sort function with range calculation and empty bucket initialization

--- bucket_sort.cpp
+++ bucket_sort.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+
+using namespace std;
+
+void bucketSort(vector<float>& arr, int n)
+{
+    // n tane boş kova oluşturuyoruz
+    vector<float> buckets[n];
+    float minVal = arr[0];
+    float maxVal = arr[0];
+    
+    for (int i = 0; i< arr.size();i++)
+    {
+        if (arr[i] < minVal)
+            minVal = arr[i];
+        if (arr[i] > maxVal)
+            maxVal = arr[i];
+    }
+
+    float range = maxVal - minVal;
+
+    // eğer tüm değerler birbirine eşitse sıralıdır diyip return yapıyoruz
+    if (range == 0)
+        return ;
+
+}
